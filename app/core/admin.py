7fbc0239e7a1d8eb85358d76186b0a7f71f0851b
Muhammad <ibneabdullatifjud@gmail.com>
2025-04-25 17:34:40 +0500

    create Admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,26 @@
-from django.contrib import admin # noqa
+"""Django admin customization"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users"""
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Personal Info', {'fields': ('name',)}),
+    )
+
+    add_fieldsets = (  # <- fixed this line
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 'password1', 'password2', 'is_active', 'is_staff', 'is_superuser'
+            )
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin)
